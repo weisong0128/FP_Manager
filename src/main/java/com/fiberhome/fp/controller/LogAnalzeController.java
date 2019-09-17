f47cd1fc766cd9a88000f5d143e9345ab521e165
zhouchao201812 <http://gitlab.njsecnet.com/zhouchao201812/FP.git>
2019-09-17 09:48:46 +0800

    新增退出，用户信息存入session

---
+++ src/main/java/com/fiberhome/fp/controller/LogAnalzeController.java
@@ -0,0 +1,319 @@
+package com.fiberhome.fp.controller;
+
+import com.fiberhome.fp.listener.event.AnalyseProcess;
+import com.fiberhome.fp.pojo.ErrorResult;
+import com.fiberhome.fp.pojo.FpOperationTable;
+import com.fiberhome.fp.pojo.LogAnalze;
+import com.fiberhome.fp.service.LogAnalyzeService;
+import com.fiberhome.fp.util.FileUtil;
+import com.fiberhome.fp.util.Page;
+import com.fiberhome.fp.util.Response;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.*;
+
+@RestController
+@RequestMapping("/log/")
+public class LogAnalzeController {
+
+   static Logger logging = LoggerFactory.getLogger(FpProjectController.class);
+
+    private   Map<String, AnalyseProcess> map = AnalyseProcess.getMap();
+
+
+    @Value("${upload.log.path}")
+    private String uploadLogPath;
+    @Autowired
+    private LogAnalyzeService logAnalyzeService;
+
+    /**
+     * 上传日志文件接口
+     *
+     * @return
+     */
+
+    @ApiOperation(value = "日志文件上传接口", notes = "上传单个日志文件")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "multipartFiles", value = "上传文件", required = true, dataType = "MultipartFile"),
+            @ApiImplicitParam(name = "projectName", value = "项目名称", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "projectLocation", value = "项目地市", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "createTime", value = "时间戳,所有上传的日志用一个时间戳", required = true, dataType = "String")}
+    )
+    @PostMapping(value = "/batchLogUpload")
+    public Response batchLogUpload(@RequestParam("file") MultipartFile[] file, String projectName, String projectLocation, String createTime) {
+        File director = new File(uploadLogPath);
+        if (!director.exists()) {
+            director.mkdirs();
+        }
+        for (MultipartFile multipartFile : file) {
+            String fileName = multipartFile.getOriginalFilename();
+            if (!fileName.toLowerCase().contains("cl.log")) {
+                return Response.error("文件命名格式应该为：cl.log*  ！");
+            }
+
+            String pt = uploadLogPath + File.separator + projectLocation + File.separator + projectName + File.separator + createTime;
+            File ptFile = new File(pt);
+            if (!ptFile.exists()) {
+                ptFile.mkdirs();
+            }
+            File file1 = new File(pt + File.separator + fileName);
+            try {
+                multipartFile.transferTo(file1);
+                logging.info("日志文件上传至 {}", pt);
+                return Response.ok();
+            } catch (Exception e) {
+                logging.error(e.getMessage(),e);
+                logging.error(e.toString());
+                return Response.error("上传失败！");
+            }
+        }
+        return Response.error();
+    }
+
+    /**
+     * 开始分析按钮
+     */
+
+    @ApiOperation(value = "开始分析按钮", notes = "开始分析按钮")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "projectName", value = "项目名称", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "projectLocation", value = "项目地市", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "createTime", value = "时间戳,与日志上传用一个时间戳", required = true, dataType = "String")}
+    )
+    @GetMapping("/startAnalyse")
+    public Response startAnalyse(String projectName, String projectLocation, String createTime, String userId) {
+        String uuid = UUID.randomUUID().toString();
+        long timeLong = Long.parseLong(createTime);
+        LogAnalze logAnalze = new LogAnalze(projectName, projectLocation, timeLong, userId);
+        logAnalze.setUuid(uuid);
+
+        try {
+            logAnalyzeService.createLogAnalze(logAnalze);
+            logAnalyzeService.startAnalyse(projectName, projectLocation, uuid, timeLong);
+        } catch (Exception e) {
+            logging.error("分析日志失败");
+            logging.error(e.getMessage(),e);
+            logging.error(e.getCause().toString());
+            return Response.error("分析日志失败");
+        }
+
+        HashMap<String, String> returnMap = new HashMap<>();
+        returnMap.put("uuid", uuid);
+        return Response.ok(returnMap);
+    }
+
+
+    /**
+     * 轮训查看进程
+     */
+    @ApiOperation(value = "轮训查看进程", notes = "轮训查看进程,用来返回进程和是否可查看结果   json字符串数组格式发送")
+    @PostMapping("/getAnalyseProcess")
+    public Response getAnalyseProcess(@RequestBody String uuids) {
+        uuids = uuids.replace("\"", "");
+        String[] split = uuids.split(",");
+        HashMap<String, Map<String, Object>> returnMap = new HashMap<>();
+        try {
+            for (String uuid : split) {
+                AnalyseProcess analyseProcess = map.get(uuid);
+                if (analyseProcess != null && analyseProcess.getFileMap() != null) {
+                    HashMap<String, Object> map1 = new HashMap<>();
+                    int process = analyseProcess.getProcess();
+                    boolean show = analyseProcess.isShow();
+                    List<String> errorResultList = analyseProcess.getErrorResultList();
+                    boolean analyseError = analyseProcess.isAnalyseError();
+                    map1.put("process", process);
+                    map1.put("show", show);
+                    map1.put("isError", analyseError);
+                    ArrayList<String> list = new ArrayList<>();
+                    for (String s : errorResultList) {
+                        if (!list.contains(s)) {
+                            list.add(s);
+                        }
+                    }
+                    map1.put("errorResultList", list);
+                    returnMap.put(uuid, map1);
+                }
+            }
+        } catch (Exception e) {
+            logging.error(e.getMessage(),e);
+            logging.error(e.getCause().toString());
+            Response.error();
+        }
+        return Response.ok(returnMap);
+    }
+
+
+    /**
+     * 根据条件返回分析list
+     */
+    @ApiOperation(value = "根据条件返回分析分析list", notes = "根据条件返回分析list")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "projectName", value = "项目名称", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "address", value = "项目地市", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "starTime", value = "分析开始时间范围 时间戳的字符串形式", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "endTime", value = "分析开始结束范围 时间戳的字符串形式", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "pageSize", value = "分页大小", required = false, dataType = "int"),
+            @ApiImplicitParam(name = "pageNo", value = "现在页数", required = false, dataType = "int")
+    }
+    )
+    @GetMapping("/getLogListByParam")
+    public Response getLogListByParam(Page page, LogAnalze logAnalze) {
+        for (Map.Entry<String, AnalyseProcess> entry : map.entrySet()) {
+            String uuid = entry.getKey();
+            AnalyseProcess analyseProcess = entry.getValue();
+            if (analyseProcess.isFinish() || analyseProcess.getProcess() == 100) {
+                map.remove(uuid);
+                continue;
+            }
+        }
+        HashMap<String, Object> hashMap = null;
+        try {
+            List<LogAnalze> logAnalyseList = logAnalyzeService.findLogAnalyseList(logAnalze, page);
+            hashMap = new HashMap<>();
+            hashMap.put("page", page);
+            hashMap.put("data", logAnalyseList);
+        } catch (Exception e) {
+            logging.error(e.getMessage(),e);
+            Response.error();
+        }
+        return Response.ok(hashMap);
+    }
+
+    /**
+     * 根据条件获取分析错误sql列表
+     * param 项目名称,项目地点 , 分析时间 , 关键字 ,时间范围,是否去重, sql错误类型
+     */
+    @ApiOperation(value = "根据条件获取分析错误sql列表", notes = "根据条件获取分析错误sql列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "pjName", value = "项目名称", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "pjLocation", value = "项目地市", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "captureTime", value = "创建记录时间 ,由前一列表的createTime字段决定", required = true, dataType = "String"),
+
+            @ApiImplicitParam(name = "timeTag", value = "时间 today,seven,halfMonth,customZone,all ", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "startTime", value = "当timeTag为customZone时,starTime生效,  开始时间戳字符串 ", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "endTime", value = "当timeTag为customZone时,endTime生效,  开始时间戳字符串", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "tag", value = "不合格原因 未加limit,limit超限制 当传入多个时,用 , 分割", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "isDistinct", value = "是否去重  0 不去重 1 去重  此功能还未实现", required = false, dataType = "int"),
+            @ApiImplicitParam(name = "keyWord", value = "关键字过滤", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "pageSize", value = "分页大小", required = false, dataType = "int"),
+            @ApiImplicitParam(name = "pageNo", value = "现在页数", required = false, dataType = "int")
+    }
+    )
+    @GetMapping("/getSqlErroListByParam")
+    public Response getSqlErroListByParam(Page page, ErrorResult errorResult) {
+        Map<String, Object> retMap = null;
+        try {
+            retMap = new HashMap<>();
+            retMap.put("page", page);
+            retMap.put("errResult", logAnalyzeService.listErrorResult(page, errorResult));
+        } catch (Exception e) {
+            logging.error(e.getMessage(),e);
+            Response.error();
+        }
+        return Response.ok(retMap);
+    }
+
+
+    /**
+     * 根据条件获取分析错误日志列表
+     * param 项目名称,项目地点 , 分析时间 , 关键字 ,时间范围,错误类型
+     */
+    @ApiOperation(value = "根据条件获取分析错误日志列表", notes = "根据条件返回分析list")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "projectName", value = "项目名称", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "projectLocation", value = "项目地市", required = true, dataType = "String"),
+            @ApiImplicitParam(name = "captureTime", value = "创建记录时间 ,由前一列表的createTime字段决定", required = true, dataType = "String"),
+
+            @ApiImplicitParam(name = "timeTag", value = "时间 today,seven,halfMonth,customZone,all ", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "startTime", value = "当timeTag为customZone时,starTime生效,  开始时间戳字符串 ", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "endTime", value = "当timeTag为customZone时,endTime生效,  开始时间戳字符串", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "tag", value = "日志等级 CRIT ERRO WARN INFO  当传入多个时,用 , 分割", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "isDistinct", value = "是否去重  0 不去重 1 去重  此功能还未实现", required = false, dataType = "int"),
+            @ApiImplicitParam(name = "keyWord", value = "关键字过滤", required = false, dataType = "String"),
+            @ApiImplicitParam(name = "pageSize", value = "分页大小", required = false, dataType = "int"),
+            @ApiImplicitParam(name = "pageNo", value = "现在页数", required = false, dataType = "int")
+    }
+    )
+    @GetMapping("/getErroListByParam")
+    public Response getErroListByParam(Page page, FpOperationTable fpOperationTable) {
+        Map<String, Object> retMap = null;
+        try {
+            List<FpOperationTable> fpOperationTables = logAnalyzeService.listOperation(page, fpOperationTable);
+            for (FpOperationTable operationTable : fpOperationTables) {
+                String errInfo = operationTable.getErrInfo();
+                if (errInfo.contains("ERRO")) {
+                    operationTable.setLogLeave("中度");
+                } else if (errInfo.contains("CRIT")) {
+                    operationTable.setLogLeave("重度");
+                } else if (errInfo.contains("WARN")) {
+                    operationTable.setLogLeave("轻度");
+                } else if (errInfo.contains("INFO")) {
+                    operationTable.setLogLeave("环境状态");
+                }
+            }
+            retMap = new HashMap<>();
+            retMap.put("page", page);
+            retMap.put("operation", fpOperationTables);
+        } catch (Exception e) {
+            logging.error(e.getMessage(),e);
+            Response.error();
+        }
+        return Response.ok(retMap);
+    }
+
+    @PostMapping("/batchDeleteLogAnaylse")
+    public Response batchDeleteLogAnaylse(@RequestBody String uuids) {
+        uuids = uuids.replace("\"", "");
+        String[] split = uuids.split(",");
+        ArrayList<String> list = new ArrayList<>();
+        for (String s : split) {
+            list.add(s);
+        }
+        try {
+            Boolean aBoolean = logAnalyzeService.batchDeleteLogAnaylse(list);
+            if (!aBoolean) {
+                return Response.error("删除失败");
+            }
+        } catch (Exception e) {
+            logging.error(e.getMessage(),e);
+            return Response.error("删除失败");
+        }
+
+        return Response.ok();
+    }
+
+    @GetMapping("/restartAnalyse")
+    public Response restartAnalyse(String pjName, String pjLocation, String createTime, String uuid) {
+        //  LogAnalze logAnalyse = logAnalyzeService.findOneLogAnalyse(uuid);
+        String pt = uploadLogPath + File.separator + pjLocation + File.separator + pjName + File.separator + createTime;
+        AnalyseProcess analyseProcess = map.get(uuid);
+        if (analyseProcess == null) {
+            //从硬盘反序列化对象,重新放进map中
+            String serializePath = pt + File.separator + uuid + "_Serialize";
+            boolean fileExixts = FileUtil.isFileExixts(serializePath);
+            if (fileExixts) {
+                try {
+                    analyseProcess = (AnalyseProcess) FileUtil.objectInputStreamDisk(serializePath);
+                    map.put(uuid, analyseProcess);
+                    logAnalyzeService.upload(analyseProcess);
+                } catch (Exception e) {
+                    logging.error(e.getMessage(),e);
+                    logging.error("重新分析失败");
+
+                }
+            } else {
+                return Response.error("序列化文件不存在,无法重新分析.请重新上传日志文件");
+            }
+        }
+        return Response.ok();
+    }
+}
---
+++ src/main/java/com/fiberhome/fp/service/impl/LogAnalyzeServiceImpl.java
@@ -0,0 +1,238 @@
+package com.fiberhome.fp.service.impl;
+
+import com.fiberhome.fp.dao.LogAnalzeDao;
+import com.fiberhome.fp.listener.event.AnalyseProcess;
+import com.fiberhome.fp.listener.event.FileStatus;
+import com.fiberhome.fp.pojo.ErrorResult;
+import com.fiberhome.fp.pojo.FpOperationTable;
+import com.fiberhome.fp.pojo.LogAnalze;
+import com.fiberhome.fp.service.FpProjectService;
+import com.fiberhome.fp.service.LogAnalyzeService;
+import com.fiberhome.fp.util.FileUtil;
+import com.fiberhome.fp.util.Page;
+import com.fiberhome.fp.util.Response;
+import com.fiberhome.fp.util.ShellUtil;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
+
+@Service
+public class LogAnalyzeServiceImpl implements LogAnalyzeService {
+
+
+    static Logger logging = LoggerFactory.getLogger(LogAnalyzeServiceImpl.class);
+    @Autowired
+    private LogAnalzeDao logAnalzeDao;
+    @Autowired
+
+    private FpProjectService fpProjectService;
+
+    private ThreadPoolExecutor pool = new ThreadPoolExecutor(20, Integer.MAX_VALUE, 60L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>());
+
+    @Value("${upload.log.path}")
+    private String uploadLogPath;
+    @Autowired
+    private AnalyseProcess analyseProcess;
+
+    @Value("${update.conf.name}")
+    String config;
+
+    @Value("${update.conf.path}")
+    String path;
+
+    @Value("${shell.execute.path}")
+    String shellPath;
+
+    @Value("${cut.file.size}")
+    String cutfilesize;
+
+
+    private Map<String, AnalyseProcess> map = AnalyseProcess.getMap();
+
+    @Override
+    public Response getLogListByParam(Page page, LogAnalze logAnalze) {
+        return null;
+    }
+
+
+    @Override
+    public boolean createLogAnalze(LogAnalze logAnalze) {
+        return logAnalzeDao.createLogAnalze(logAnalze);
+    }
+
+    @Override
+    public List<LogAnalze> findLogAnalyseList(LogAnalze param, Page page) {
+        if (StringUtils.isNotEmpty(param.getProjectName())) {
+            String[] arrays = param.getProjectName().split(",");
+            param.setProjectNameList(new ArrayList<>(Arrays.asList(arrays)));
+        }
+        if (StringUtils.isNotEmpty(param.getAddress())) {
+            String[] arrays = param.getAddress().split(",");
+            param.setAddressList(new ArrayList<>(Arrays.asList(arrays)));
+        }
+        return logAnalzeDao.findLogAnalyseList(param, page);
+    }
+
+
+    @Override
+    public boolean startAnalyse(String project, String location, String uuid, Long analyseTime) {
+        String dir = uploadLogPath + File.separator + location + File.separator + project + File.separator + analyseTime;
+        File direct = new File(dir);
+        ArrayList<File> fileArrayList1 = new ArrayList<>();
+        Long byteSize = FileUtil.getByteSize(Integer.valueOf(cutfilesize));
+        AnalyseProcess.fileSizeInit(uuid, byteSize);
+        FileUtil.findAllSizeMore(direct, uuid);
+        FileUtil.getSizeLLesser(direct, fileArrayList1, uuid);
+        AnalyseProcess.init(uuid, fileArrayList1, project, location, analyseTime, dir);
+        upload(map.get(uuid));
+        return true;
+    }
+
+
+    public void upload(AnalyseProcess analyseProcess) {
+        String dir = uploadLogPath + File.separator +
+                analyseProcess.getProjectLocation() + File.separator +
+                analyseProcess.getProjectName() + File.separator +
+                analyseProcess.getCreateTime();
+        ConcurrentMap<String, FileStatus> fileMap;
+        if (analyseProcess.getFileMap().size() != analyseProcess.getUnSuccessFileMap().size()) {
+            fileMap = analyseProcess.getUnSuccessFileMap();
+            int finishCount = analyseProcess.getFinishCount();
+            analyseProcess.setFinishCount(finishCount - fileMap.size());
+        } else {
+            fileMap = analyseProcess.getFileMap();
+        }
+        String uuid = analyseProcess.getUuid();
+        String uploadFileRootPath = analyseProcess.getUploadFileRootPath();
+        List<File> fileArrayList1 = map2FileList(fileMap);
+        for (int i = 0; i < fileArrayList1.size(); i++) {
+            File file = fileArrayList1.get(i);
+            String filePath = file.getPath();
+            Long createTime = analyseProcess.getCreateTime();
+            String projectName = analyseProcess.getProjectName();
+            String projectLocation = analyseProcess.getProjectLocation();
+            String j = i + "";
+            pool.execute(() -> {
+                //调用脚本方法
+                String bashCommand = "sh " + shellPath + File.separator + "fp_analysis.sh  " + filePath + " " + projectName + " " + projectLocation + " " + createTime;
+                //   if (!j.equals("1")) {
+                ShellUtil.newShSuccess(bashCommand, uuid, filePath);
+                //   }
+                AnalyseProcess analyseProcess1 = map.get(uuid);
+                FileStatus fileStatus = analyseProcess1.getFileMap().get(filePath);
+                LogAnalze logAnalze = fileStatus.setFinish(true);
+                if (logAnalze != null) {
+                    //如果本次分析完整
+                    String objectSerializePath = analyseProcess1.getObjectSerializePath(dir);
+                    try {
+                        if (analyseProcess1.getUnSuccessFileMap().size() == 0) {
+                            // File file1 = new File(objectSerializePath);
+                            FileUtil.deleteRootPathDir(new File(uploadFileRootPath), "_cut");
+                        } else {
+                            FileUtil.objectOutputStreamDisk(analyseProcess, objectSerializePath);
+                        }
+                        logAnalzeDao.updateLogAnalze(logAnalze);
+                    } catch (Exception e) {
+                        logging.error(e.getMessage(), e);
+                    }
+                }
+            });
+            sleep(1100);
+        }
+    }
+
+
+    public void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+
+    @Override
+    public List<ErrorResult> listErrorResult(Page page, ErrorResult errorResult) {
+        if (StringUtils.isNotEmpty(errorResult.getPjName()) && !errorResult.getPjName().equalsIgnoreCase("")) {
+            String[] pjNames = errorResult.getPjName().split(",");
+            errorResult.setPjNameList(new ArrayList<>(Arrays.asList(pjNames)));
+        }
+        if (StringUtils.isNotEmpty(errorResult.getPjLocation()) && !errorResult.getPjLocation().equalsIgnoreCase("")) {
+            String[] pjLocations = errorResult.getPjLocation().split(",");
+            errorResult.setPjLocationList(new ArrayList<>(Arrays.asList(pjLocations)));
+        }
+        if (StringUtils.isNotEmpty(errorResult.getTag()) && !errorResult.getTag().equalsIgnoreCase("")) {
+            String[] tagList = errorResult.getTag().split(",");
+            errorResult.setTagList(new ArrayList<>(Arrays.asList(tagList)));
+        }
+        return logAnalzeDao.listErrResult(page, errorResult);
+    }
+
+    @Override
+    public List<FpOperationTable> listOperation(Page page, FpOperationTable fpOperationTable) {
+        if (StringUtils.isNotEmpty(fpOperationTable.getPjName())) {
+            String[] pjNames = fpOperationTable.getPjName().split(",");
+            fpOperationTable.setPjNameList(new ArrayList<>(Arrays.asList(pjNames)));
+        }
+        if (StringUtils.isNotEmpty(fpOperationTable.getPjLocation())) {
+            String[] pjLocations = fpOperationTable.getPjLocation().split(",");
+            fpOperationTable.setPjLocationList(new ArrayList<>(Arrays.asList(pjLocations)));
+        }
+
+        if (StringUtils.isNotEmpty(fpOperationTable.getLogLeave())) {
+            String[] logLeaveList = fpOperationTable.getLogLeave().split(",");
+            fpOperationTable.setLogLeaveList(new ArrayList<>(Arrays.asList(logLeaveList)));
+        }
+        return logAnalzeDao.list(page, fpOperationTable);
+    }
+
+    @Override
+    public Boolean batchDeleteLogAnaylse(List<String> uuids) {
+        for (String uuid : uuids) {
+            try {
+                if (map.containsKey(uuid)) {
+                    map.remove(uuid);
+                }
+                LogAnalze oneLogAnalyse = logAnalzeDao.findOneLogAnalyse(uuid);
+                String projectLocation = oneLogAnalyse.getAddress();
+                String projectName = oneLogAnalyse.getProjectName();
+                Long createTime = oneLogAnalyse.getCreateTime();
+                String uploadFileRootPath = uploadLogPath + File.separator + projectLocation + File.separator + projectName + File.separator + createTime;
+                FileUtil.deleteRootPathDir(new File(uploadFileRootPath), "_cut");
+                logAnalzeDao.deleteLogAnalze(uuid);
+            } catch (Exception e) {
+                logging.error(e.getMessage(), e);
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private List<File> map2FileList(Map<String, FileStatus> fileMap) {
+        ArrayList<File> list = new ArrayList<>();
+        for (Map.Entry<String, FileStatus> entry : fileMap.entrySet()) {
+            File file = new File(entry.getValue().getFilePath());
+            if (file.exists()) {
+                list.add(file);
+            }
+        }
+        return list;
+    }
+
+    public LogAnalze findOneLogAnalyse(String uuid) {
+        return logAnalzeDao.findOneLogAnalyse(uuid);
+    }
+
+
+}
